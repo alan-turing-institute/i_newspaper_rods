854d5c3b0a4e8ecfbec966b75cf6413027e0870f
Mike Jackson <michaelj@epcc.ed.ac.uk>
2018-07-06 14:48:12 +0100

    Updated newrods/sparkrods.py to handle both local files and URLs in oids.txt. Added open_stream(url) which opens url via requests.get, gets raw stream then sets decode_content = True. get_streams behaves conditionally on whether all oids are prefixed with http://|https:// or are local files. Removed prefixing of URL's by UCL-specific URL. This should be responsibility of  modules, to set up oids.txt correctly.

--- newsrods/sparkrods.py
+++ newsrods/sparkrods.py
@@ -1,22 +1,31 @@
 '''
-Module to load and read the files using spark
+Module to load and read the files using Spark.
 '''
 
 from newsrods.issue import Issue
 from requests import get
 
-DATA_STORE_URL = "utilities.rd.ucl.ac.uk"
+
+def open_stream(url):
+    stream = get(url, stream=True)  # requests.models.Response
+    raw = stream.raw  # urllib3.response.HTTPResponse
+    raw.decode_content = True
+    return raw
 
 
-def get_streams(context, source="oids.txt"):
+def get_streams(context,
+                source="oids.txt"):
     '''
-    Turn a list of oids in a file into a RDD of Issues
+    Turn a list of oids in a file into a RDD of Issues.
     '''
     oids = [oid.strip() for oid in list(open(source))]
-
+    are_urls = len(oids) > 0 and \
+        (oids[0].lower().startswith("http://") or \
+         oids[0].lower().startswith("https://"))
     rddoids = context.parallelize(oids)
-    issues = rddoids.map(lambda (oid): 'http://' + DATA_STORE_URL +
-                         '/objects/' + oid) \
-                    .map(lambda url: get(url, stream=True)) \
-                    .map(lambda stream: Issue(stream.raw))
+    if (are_urls):
+        issues = rddoids.map(lambda url: open_stream(url)) \
+                            .map(lambda raw: Issue(raw))
+    else:  # file paths
+        issues = rddoids.map(lambda file_name: Issue(file_name))
     return issues
