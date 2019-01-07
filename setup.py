37cd42eb32f8eae38a385e48653be99773a1ba27
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-01-07 17:43:55 +0000

    Updated setup.py with current authors and version

--- setup.py
+++ setup.py
@@ -13,20 +13,32 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+AUTHORS = ["James Hetherington",
+           "Raquel Alegre",
+           "Roma Klapaukh",
+           "Mike Jackson",
+           "Rosa Filgueira"]
+
+EMAILS = ["j.hetherington@ucl.ac.uk",
+          "r.alegre@ucl.ac.uk",
+          "r.klapaukh@ucl.ac.uk>",
+          "michaelj@epcc.ed.ac.uk",
+          "rosa.filgueira@ed.ac.uk"]
+
 setup(
     name="iNewsRods",
     version="0.0.1",
-    author="James Hetherington",
-    author_email="j.hetherington@ucl.ac.uk",
+    authors=", ".join(AUTHORS),
+    author_email=", ".join(EMAILS),
     description=(
-        "Harness for Apache Spark analysis of newspaper corpus"),
+        "Harness for Apache Spark analysis of newspapers corpus"),
     license="MIT",
     keywords="digital humanities research newspapers",
     url="https://github.com/alan-turing-institute/i_newspaper_rods",
     packages=['newsrods'],
     long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Topic :: Research :: Humanities",
         "License :: OSI Approved :: MIT License",
     ]
